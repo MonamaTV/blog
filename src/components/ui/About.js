2b642ce469da223b91e93de6a83f1b213b6a54e5
MonamaTV <keletsovincent92@gmail.com>
2022-02-21 19:13:13 +0200

    third commit

--- src/components/ui/About.js
+++ src/components/ui/About.js
@@ -15,7 +15,7 @@ const About = () => {
                     <h2>Small Piece About Me</h2>
                     <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, without the meaning of the text influencing the design</p>
                     <div className={styles.links}>
-                        <a target="_blank" href="https://www.linkedin.com/in/vincent-monama-b5705b18b/" className={styles.linkedin}>Check My LinkedIn</a>
+                        <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/vincent-monama-b5705b18b/" className={styles.linkedin}>Check My LinkedIn</a>
                         <a className={styles.contactLink} href="#contacts">Contact</a>
                     </div>
                 </div>

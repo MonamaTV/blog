f826fe169537ff8794d81205f5a021a93bf5bbaf
MonamaTV <keletsovincent92@gmail.com>
2022-03-05 19:09:32 +0200

    wraping up the design

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -12,12 +12,13 @@ const Blog = () => {
                 <div className="blog-header">
                     {/* The title of the article and the author */}
                     <h1>Improve Your Productivity When Coding</h1>
-                    <small>Written by Monama Tadima</small>
+                    <small>By Monama Tadima</small>
                 </div>
                 <div className="content-image">
                     <Image width={"1000"} height={"440"} src="/display.jpg" />
                 </div>
                 <div className="content-blog">
+                    <small>27 March 2022</small>
                     <h2>Awareness</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel assumenda qui minima corporis eligendi iste quis obcaecati. Aperiam, quod. Atque consequuntur tempore esse itaque obcaecati, vero ratione dolore, amet laborum fuga natus. Dignissimos consequuntur iure, beatae nobis, voluptatum saepe praesentium error, asperiores voluptates nam sed! Eaque atque soluta perferendis ab.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel assumenda qui minima corporis eligendi iste quis obcaecati. Aperiam, quod. Atque consequuntur tempore esse itaque obcaecati, vero ratione dolore, amet laborum fuga natus. Dignissimos consequuntur iure, beatae nobis, voluptatum saepe praesentium error, asperiores voluptates nam sed! Eaque atque soluta perferendis ab.</p>

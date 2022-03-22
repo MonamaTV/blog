0c23411f4782df4ac3620cacac16d6e7b1fef0ab
MonamaTV <keletsovincent92@gmail.com>
2022-03-22 23:18:54 +0200

    added the blog the landing page

--- src/components/ui/About.js
+++ src/components/ui/About.js
@@ -13,7 +13,7 @@ const About = () => {
                 </div>
                 <div className={styles.aboutContent}>
                     <h2>Small Piece About Me</h2>
-                    <p>My motivation in software development has led me to become a solution-driven person who always thrives to solve problems with the best possible methodologies. I mostly experience growth in spaces where I get challenged to do tasks that are out of my 'comfort-zone'. Also, environments that allow collaborative efforts tend to be my favorites because you learn a lot more from working with people with different experiences and skillsets. </p>  
+                    <p>My motivation in software development has led me to become a solution-driven person who always thrives to solve problems with the best possible methodologies. I mostly experience growth in spaces where I get challenged to do tasks that are out of my &apos;comfort-zone&apos;. Also, environments that allow collaborative efforts tend to be my favorites because you learn a lot more from working with people with different experiences and skillsets. </p>  
                     <div className={styles.links}>
                         <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/vincent-monama-b5705b18b/" className={styles.linkedin}>Check My LinkedIn</a>
                         <a className={styles.contactLink} href="#contacts">Contact</a>

f58402b70c8873bbd5ea8598a5e9324e0aef9570
MonamaTV <keletsovincent92@gmail.com>
2022-03-22 23:49:35 +0200

    added the not found page

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,21 @@
-
+import Image from "next/image";
+import Link from "next/link";
+import styles from '../styles/404.module.css';
 const CustomNotFound = () => {
 
     return (
-        <div className="container">
-            <h1>Not found</h1>
+        <div className={styles.not_found}>
+            <div className="not-found-image">
+                <Image  width={"1000"} height={"440"} src="/notfound.svg" />
+            </div>
+            <div className="not-found-content">
+                <h1>PAGE NOT FOUND</h1>
+                <p>Sorry, we couldn't find the page you are looking for</p>
+                <div className={styles.not_found_links}>
+                    <Link href="/"><a>Home</a></Link>
+                    <Link href="/blog"><a>Visit Blog</a></Link>
+                </div>
+            </div>
         </div>
     );
 };
